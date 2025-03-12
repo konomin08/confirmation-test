ceba84fdffb084688e67e7a43e4872ebec37f903
konomin <zako606606@gmail.com>
2025-03-12 14:51:56 +0900

    バリテーションの土台作成

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\ContactRequest;
 use App\Models\Contact;
 
 class ContactController extends Controller
@@ -12,13 +12,13 @@ class ContactController extends Controller
         return view('index');
     }
     //contact.blade.php（フォーム入力ページ）を呼び出す
-    public function confirm(Request $request)
+    public function confirm(ContactRequest $request)
     {
         $contact = $request->only(['name', 'email', 'tel', 'content']);
         return view('confirm', ['contact' => $contact]);
     // 問い合わせフォームの送信ボタンクリック時に行われる処理
     }
-    public function store(Request $request)
+    public function store(ContactRequest $request)
     {
         $contact = $request->only(['name', 'email', 'tel', 'content']);
         Contact::create($contact);
--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -34,10 +34,13 @@
           </div>
           <div class="form__group-content">
             <div class="form__input--text">
-              <input type="text" name="name" placeholder="テスト太郎" />
+              <input type="text" name="name" placeholder="テスト太郎" value="{{ old('name') }}" />
             </div>
             <div class="form__error">
-              <!--バリデーション機能を実装したら記述します。-->
+              @error('name')
+              {{ $message }}
+              @enderror
+              <!--バリデーション機能実装-->
             </div>
           </div>
         </div>
@@ -48,10 +51,13 @@
           </div>
           <div class="form__group-content">
             <div class="form__input--text">
-              <input type="email" name="email" placeholder="test@example.com" />
+              <input type="email" name="email" placeholder="test@example.com" value="{{ old('email') }}" />
             </div>
             <div class="form__error">
-              <!--バリデーション機能を実装したら記述します。-->
+              @error('email')
+              {{ $message }}
+              @enderror
+              <!--バリデーション機能実装-->
             </div>
           </div>
         </div>
@@ -62,10 +68,13 @@
           </div>
           <div class="form__group-content">
             <div class="form__input--text">
-              <input type="tel" name="tel" placeholder="09012345678" />
+              <input type="tel" name="tel" placeholder="09012345678" value="{{ old('tel') }}" />
             </div>
             <div class="form__error">
-              <!--バリデーション機能を実装したら記述します。-->
+              @error('tel')
+              {{ $message }}
+              @enderror
+              <!--バリデーション機能実装-->
             </div>
           </div>
         </div>
